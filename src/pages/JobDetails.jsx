c4e6fe8730c00f2637c8a37cf1de1b5dfaf07e38
Mohsanul786 <mdmohsan2407@gmail.com>
2025-01-30 20:40:30 +0530

    err: job postingform error solved

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,49 +1,61 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const JobDetails = () => {
   const { jobId } = useParams();
   const { jobs } = useSelector((state) => state.jobs);
+  const { isOpen } = useSelector((state) => state.navbar);
+
   const findJob = jobs.find((item) => item._id === jobId);
 
   return (
     <>
-      <div className="max-w-6xl min-h-screen pt-20 mx-auto">
-        <h1 className="text-2xl font-medium py-4  mx-5">{findJob?.jobTitle}</h1>
-        <div className="p-5 border mx-5 border-gray-300 rounded-md flex flex-col gap-4">
-          <p>
-            <span className="font-medium">Company Name: </span>{" "}
-            {findJob?.companyName}
-          </p>
-          <p>
-            <span className="font-medium">Location: </span>
-            {findJob?.location}
-          </p>
-          <p>
-            <span className="font-medium">Salary: </span>
-            {findJob?.salary}
-          </p>
-          <p>
-            <span className="font-medium">Job Type: </span>
-            {findJob?.jobType}
-          </p>
-          <p>
-            <span className="font-medium">Job Description: </span>
-            {findJob?.jobDescription}
-          </p>
-          <p>
-            <span className="font-medium">Qualifications: </span>
-            <ol type="1">
-              {findJob?.requiredQualifications.split(",").map((item, i) => (
-                <li key={i}>
-                  <span className="">{i + 1}. </span> {item}
-                </li>
-              ))}
-            </ol>
-          </p>{" "}
+      <motion.div
+        style={{ marginTop: isOpen ? "160px" : "0px" }}
+        // animate={{ marginTop: isOpen ? 120 : 0 }}
+        transition={{ duration: 0.9, ease: "easeInOut", times: [0, 0.5, 1] }}
+        className="p-6 mt-[64px]"
+      >
+        <div className="max-w-6xl min-h-screen pt-10 mx-auto">
+          <h1 className="text-2xl font-medium py-4  mx-5">
+            {findJob?.jobTitle}
+          </h1>
+          <div className="p-5 border mx-5 border-gray-300 rounded-md flex flex-col gap-4">
+            <p>
+              <span className="font-medium">Company Name: </span>{" "}
+              {findJob?.companyName}
+            </p>
+            <p>
+              <span className="font-medium">Location: </span>
+              {findJob?.location}
+            </p>
+            <p>
+              <span className="font-medium">Salary: </span>
+              {findJob?.salary}
+            </p>
+            <p>
+              <span className="font-medium">Job Type: </span>
+              {findJob?.jobType}
+            </p>
+            <p>
+              <span className="font-medium">Job Description: </span>
+              {findJob?.jobDescription}
+            </p>
+            <p>
+              <span className="font-medium">Qualifications: </span>
+              <ol type="1">
+                {findJob?.requiredQualifications.split(",").map((item, i) => (
+                  <li key={i}>
+                    <span className="">{i + 1}. </span> {item}
+                  </li>
+                ))}
+              </ol>
+            </p>{" "}
+          </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
--- src/pages/PostJob.jsx
+++ src/pages/PostJob.jsx
@@ -65,114 +65,114 @@ const PostJob = () => {
         // animate={{ marginTop: isOpen ? 120 : 0 }}
         transition={{ duration: 0.9, ease: "easeInOut", times: [0, 0.5, 1] }}
         className="p-6 mt-[64px]"
-      ></motion.div>
-      <div className="max-w-6xl min-h-screen mx-auto pt-5">
-        <h1 className="text-3xl font-medium pb-4 px-5 md:px-0">Post a Job</h1>
-        <ToastContainer />
-        <form onSubmit={submitHandler} className="px-5 md:px-0">
-          <div>
-            <label htmlFor="jobTitle" className="">
-              Job Title:{" "}
-            </label>
-            <input
-              type="text"
-              name="jobTitle"
-              required
-              placeholder="Software Engineer"
-              value={jobDetails.jobTitle}
-              onChange={changeHandler}
-              className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
-            />
-          </div>
-          <div>
-            <label htmlFor="companyName" className="">
-              Company Name:{" "}
-            </label>
-            <input
-              type="text"
-              name="companyName"
-              required
-              value={jobDetails.companyName}
-              onChange={changeHandler}
-              placeholder="Tecno Pvt. Ltd."
-              className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
-            />
-          </div>
-          <div>
-            <label htmlFor="salary" className="">
-              Salary:{" "}
-            </label>
-            <input
-              type="number"
-              name="salary"
-              required
-              value={jobDetails.salary}
-              onChange={changeHandler}
-              placeholder="$10000"
-              className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
-            />
-          </div>
-          <div>
-            <label htmlFor="location" className="">
-              Location:{" "}
-            </label>
-            <input
-              type="text"
-              name="location"
-              required
-              value={jobDetails.location}
-              onChange={changeHandler}
-              placeholder="Banglore"
-              className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
-            />
-          </div>
-          <div>
-            <label htmlFor="jobType" className="">
-              Job Type:{" "}
-            </label>
-            <select
-              name="jobType"
-              className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
-              value={jobDetails.jobType}
-              onChange={changeHandler}
-            >
-              <option value="Full-time (On-site)">Full-time (On-site)</option>
-              <option value="Part-time (On-site)">Part-time (On-site)</option>
-              <option value="Full-time (Remote)">Full-time (Remote)</option>
-              <option value="Part-time (Remote)">Part-time (Remote)</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="jobDescription" className="">
-              Job Description:{" "}
-            </label>
-            <textarea
-              name="jobDescription"
-              value={jobDetails.jobDescription}
-              onChange={changeHandler}
-              cols="30"
-              className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
-            ></textarea>
-          </div>
-          <div>
-            <label htmlFor="requiredQualifications" className="">
-              Job Qualifications:{" "}
-            </label>
-            <textarea
-              name="requiredQualifications"
-              cols="30"
-              value={jobDetails.requiredQualifications}
-              onChange={changeHandler}
-              className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
-            ></textarea>
-          </div>
-          <p className="py-2 text-red-600">{error}</p>
-          <button className="rounded-md py-3 bg-blue-600 text-white px-5 mb-9">
-            Post Job
-          </button>
-        </form>
-      </div>
-      <motion />
+      >
+        <div className="max-w-6xl min-h-screen mx-auto pt-5">
+          <h1 className="text-3xl font-medium pb-4 px-5 md:px-0">Post a Job</h1>
+          <ToastContainer />
+          <form onSubmit={submitHandler} className="px-5 md:px-0">
+            <div>
+              <label htmlFor="jobTitle" className="">
+                Job Title:{" "}
+              </label>
+              <input
+                type="text"
+                name="jobTitle"
+                required
+                placeholder="Software Engineer"
+                value={jobDetails.jobTitle}
+                onChange={changeHandler}
+                className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
+              />
+            </div>
+            <div>
+              <label htmlFor="companyName" className="">
+                Company Name:{" "}
+              </label>
+              <input
+                type="text"
+                name="companyName"
+                required
+                value={jobDetails.companyName}
+                onChange={changeHandler}
+                placeholder="Tecno Pvt. Ltd."
+                className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
+              />
+            </div>
+            <div>
+              <label htmlFor="salary" className="">
+                Salary:{" "}
+              </label>
+              <input
+                type="number"
+                name="salary"
+                required
+                value={jobDetails.salary}
+                onChange={changeHandler}
+                placeholder="$10000"
+                className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
+              />
+            </div>
+            <div>
+              <label htmlFor="location" className="">
+                Location:{" "}
+              </label>
+              <input
+                type="text"
+                name="location"
+                required
+                value={jobDetails.location}
+                onChange={changeHandler}
+                placeholder="Banglore"
+                className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
+              />
+            </div>
+            <div>
+              <label htmlFor="jobType" className="">
+                Job Type:{" "}
+              </label>
+              <select
+                name="jobType"
+                className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
+                value={jobDetails.jobType}
+                onChange={changeHandler}
+              >
+                <option value="Full-time (On-site)">Full-time (On-site)</option>
+                <option value="Part-time (On-site)">Part-time (On-site)</option>
+                <option value="Full-time (Remote)">Full-time (Remote)</option>
+                <option value="Part-time (Remote)">Part-time (Remote)</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="jobDescription" className="">
+                Job Description:{" "}
+              </label>
+              <textarea
+                name="jobDescription"
+                value={jobDetails.jobDescription}
+                onChange={changeHandler}
+                cols="30"
+                className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
+              ></textarea>
+            </div>
+            <div>
+              <label htmlFor="requiredQualifications" className="">
+                Job Qualifications:{" "}
+              </label>
+              <textarea
+                name="requiredQualifications"
+                cols="30"
+                value={jobDetails.requiredQualifications}
+                onChange={changeHandler}
+                className="w-full rounded-md border border-gray-300 py-2 px-3 my-2"
+              ></textarea>
+            </div>
+            <p className="py-2 text-red-600">{error}</p>
+            <button className="rounded-md py-3 bg-blue-600 text-white px-5 mb-9">
+              Post Job
+            </button>
+          </form>
+        </div>
+      </motion.div>
     </>
   );
 };
